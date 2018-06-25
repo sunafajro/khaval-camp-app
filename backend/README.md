cb76b850c6872537b418aa088b85138c5469c731
Evgeny Belkin <sunafajro@gmail.com>
2018-06-25 22:55:54 +0300

    удален sails бекенд

--- backend/README.md
+++
@@ -1,6 +0,0 @@
-# cv500
-
-a [Sails](http://sailsjs.org) application
-
-# run server
-$ sails lift
--- backend/api/services/MailerService.js
+++
@@ -1,9 +0,0 @@
-var MailerService = require("sails-service-mailer");
-
-module.exports = MailerService("sendmail", {
-  from: "no-reply@cv-haval.org",
-  subject: "Новая заявка на участие в летнем лагере Хавал!",
-  provider: {
-    path: "/usr/sbin/sendmail"
-  }
-});
